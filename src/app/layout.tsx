d827c87bb83bd2bff6c1a06339018e21b62af616
inkblot626 <inkblot626@gmail.com>
2023-12-03 17:33:04 +0900

    modify src

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const sans = Open_Sans({ subsets: ["latin"] });
 
