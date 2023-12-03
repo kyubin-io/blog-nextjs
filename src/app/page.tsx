fd2126710dee480809ffd6216241e15af0eed9b0
inkblot626 <inkblot626@gmail.com>
2023-12-03 15:52:20 +0900

    modify main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <section className="flex min-h-screen flex-col items-center justify-between">
-      main
-    </section>
-  );
+  return <section className="bg-gray-500">main</section>;
 }
