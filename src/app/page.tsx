7702de0cb8440df25c205bab7e9111fb74b4c1a5
inkblot626 <inkblot626@gmail.com>
2023-12-04 09:24:50 +0900

    structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import Hero from "../components/Hero";
+import FeaturedPosts from "@/components/FeaturedPosts";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <FeaturedPosts />
     </>
   );
 }
