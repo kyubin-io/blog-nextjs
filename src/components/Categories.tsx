ab664da4f8209126ed3162d9fb262a40460325b2
inkblot626 <inkblot626@gmail.com>
2023-12-09 14:55:24 +0900

    create post page

---
+++ src/components/Categories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+type Props = {
+  categories: string[];
+  selected: string;
+  onClick: (category: string) => void;
+};
+
+export default function Categories({ categories, selected, onClick }: Props) {
+  return (
+    <section>
+      <h2></h2>
+      <ul>
+        {categories.map((category) => (
+          <li key={category} onClick={() => onClick(category)}>
+            {category}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
