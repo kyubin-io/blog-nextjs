08832707698398aa9f756699525cfc9df0ff9eb7
inkblot626 <inkblot626@gmail.com>
2023-12-09 18:06:55 +0900

    create blog-post page

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -7,15 +7,19 @@ export type Post = {
   date: Date;
   category: string;
   path: string;
-  featured: string;
+  featured: boolean;
 };
 
-export function getFeaturedPosts() {
-  return getAllPosts().then((posts) => posts.filter((post) => post.featured));
+export type PostData = Post & { content: string };
+
+export async function getFeaturedPosts(): Promise<Post[]> {
+  return getAllPosts() //
+    .then((posts) => posts.filter((post) => post.featured));
 }
 
-export function getNonFeaturedPosts() {
-  return getAllPosts().then((posts) => posts.filter((post) => !post.featured));
+export async function getNonFeaturedPosts(): Promise<Post[]> {
+  return getAllPosts() //
+    .then((posts) => posts.filter((post) => !post.featured));
 }
 
 export async function getAllPosts(): Promise<Post[]> {
@@ -23,4 +27,15 @@ export async function getAllPosts(): Promise<Post[]> {
   return readFile(filePath, "utf-8")
     .then<Post[]>(JSON.parse)
     .then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
+}
+
+export async function getPostData(fileName: string): Promise<PostData> {
+  const filePath = path.join(process.cwd(), "data", "posts", `${fileName}.md`);
+  const metadata = await getAllPosts() //
+    .then((posts) => posts.find((post) => post.path === fileName));
+  if (!metadata)
+    throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
+
+  const content = await readFile(filePath, "utf-8");
+  return { ...metadata, content };
 }
