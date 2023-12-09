876fbe4fa8b8bb1523650785ab7ad6a0ee575b75
inkblot626 <inkblot626@gmail.com>
2023-12-09 14:27:26 +0900

    odify category

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Category() {
   return (
-    <article className="">
+    <article className="pl-4">
       <h3 className="text-xl font-bold">Category</h3>
       <hr />
       <ul className="pl-2">
