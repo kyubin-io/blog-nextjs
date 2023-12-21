f5371a51ad4d9c49d2de7b0636aa4321f58737e6
inkblot626 <inkblot626@gmail.com>
2023-12-21 15:16:04 +0900

    add handleClick function

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 const H3_CLASS = "text-white font-bold";
 const INPUT_CLASS = "mb-2 placeholder:text-black";
 
 export default function EmailForm() {
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(e);
+  };
   return (
     <form className="w-[500px] bg-slate-500 flex flex-col rounded-lg p-4">
       <h3 className={H3_CLASS}>Your Email</h3>
@@ -13,7 +19,11 @@ export default function EmailForm() {
         placeholder="Message"
         className={`${INPUT_CLASS} h-40`}
       ></textarea>
-      <button type="submit" className="text-black font-bold bg-yellow-400">
+      <button
+        type="submit"
+        className="text-black font-bold bg-yellow-400"
+        onClick={handleClick}
+      >
         Submit
       </button>
     </form>
