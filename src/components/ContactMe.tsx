41c13a5733236fdee2a3b12a8ecffe5370dd1026
inkblot626 <inkblot626@gmail.com>
2023-12-21 15:09:03 +0900

    add address

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { FaGithubSquare, FaLinkedin, FaBlogger } from "react-icons/fa";
 
 const ICON_CLASS = "text-6xl";
@@ -8,9 +9,16 @@ export default function ContactMe() {
       <h2 className="text-3xl font-bold">Contact Me</h2>
       <p>kyubin.it@google.com</p>
       <div className="flex">
-        <FaGithubSquare className={ICON_CLASS} />
-        <FaLinkedin className={ICON_CLASS} />
-        <FaBlogger className={ICON_CLASS} />
+        <Link href={"https://github.com/kyubinAgnes"}>
+          <FaGithubSquare className={ICON_CLASS} />
+        </Link>
+        <Link href={"https://www.linkedin.com/in/kyubin-kim-935788293/"}>
+          <FaLinkedin className={ICON_CLASS} />
+        </Link>
+
+        <Link href={"https://mnemoni-k.blogspot.com/"}>
+          <FaBlogger className={ICON_CLASS} />
+        </Link>
       </div>
     </div>
   );
