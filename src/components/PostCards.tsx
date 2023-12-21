1f0fd2dc960fe5471f1b4beb3605d5c10c137fc4
inkblot626 <inkblot626@gmail.com>
2023-12-21 13:02:00 +0900

    postcard component

--- src/components/PostCards.tsx
+++ src/components/PostCards.tsx
@@ -1,12 +1,10 @@
-import { getFBData } from "@/service/posts";
-import PostCard from "./PostCard";
+import PostCard from "./AdjacentPostCard";
 
 export default async function PostCards({ path }: { path: string }) {
-  const test = await getFBData(path);
   return (
     <div className="w-full h-44 flex">
-      <PostCard info={test[0]} />
-      <PostCard info={test[1]} />
+      {/* <PostCard info={test[0]} />
+      <PostCard info={test[1]} /> */}
     </div>
   );
 }
