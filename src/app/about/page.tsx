516b7c3d3c0f4c5ff928c9e9ac700dc77a353252
김규빈 <kyubin@gimgyubin-ui-MacBookAir.local>
2024-03-31 00:11:13 +0900

    add about, contact

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,29 @@
+import Hero from "@/components/Hero";
+
+const TITLE_CLASS = "text-2xl font-bold text-gray-800 my-2";
+
 export default function AboutPage() {
-  return <p>About</p>;
+  return (
+    <>
+      <Hero />
+      <section className="bg-gray-100 shadow-lg p-8 m-8 text-center">
+        <h2 className={TITLE_CLASS}>Who Am I?</h2>
+        <p>개발을 사랑하는 풀스택 개발자</p>
+        <p>사람과 디자인을 담는 웹앱을 만들고 있음</p>
+        <h2 className={TITLE_CLASS}>Career</h2>
+        <p>Gaon Platform (2020 - 2023.6)</p>
+        <p>Innogen (2017 - 2018)</p>
+        <h2 className={TITLE_CLASS}>Skills</h2>
+        <p>
+          React, Javascript, Typescript
+          <br />
+          Next.js, Python, Django
+          <br />
+          Node, Git, Clean Code
+          <br />
+          Vs code, IntelliJ, MongoDB, PostgreSQL
+        </p>
+      </section>
+    </>
+  );
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,41 @@
+import ContactForm from "@/components/ContactForm";
+import { FaGithubSquare, FaLinkedin, FaBlogger } from "react-icons/fa";
+
+const LINKS = [
+  {
+    icon: <FaGithubSquare />,
+    url: "https://github.com/kyubinAgnes",
+  },
+  {
+    icon: <FaLinkedin />,
+    url: "https://www.linkedin.com/in/kyubin-agnes/",
+  },
+  {
+    icon: <FaBlogger />,
+    url: "https://mnemoni-k.blogspot.com/",
+  },
+];
+
 export default function ContactPage() {
-  return <p>Contact</p>;
+  return (
+    <section className="flex flex-col items-center">
+      <h2 className="text-3xl font-bold my-2">Contact Me</h2>
+      <p>kyubin.it@google.com</p>
+      <ul className="flex gap-4 my-2">
+        {LINKS.map((link, index) => (
+          <a
+            key={index}
+            href={link.url}
+            target="_blank"
+            rel="noreferrer"
+            className="text-5xl hover:text-yellow-400"
+          >
+            {link.icon}
+          </a>
+        ))}
+      </ul>
+      <h2 className="text-3xl font-bold my-8">Or Send me an email</h2>
+      <ContactForm />
+    </section>
+  );
 }
