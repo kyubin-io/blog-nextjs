4166a04c539f16dcace11e28a0ab345ff4a72774
김규빈 <kyubin@gimgyubin-ui-MacBookAir.local>
2024-03-31 12:59:13 +0900

    add Form

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,8 +8,8 @@ export default function AboutPage() {
       <Hero />
       <section className="bg-gray-100 shadow-lg p-8 m-8 text-center">
         <h2 className={TITLE_CLASS}>Who Am I?</h2>
-        <p>개발을 사랑하는 풀스택 개발자</p>
-        <p>사람과 디자인을 담는 웹앱을 만들고 있음</p>
+        <p>A full-stack developer who loves coding</p>
+        <p>Creating web apps that involve people and design</p>
         <h2 className={TITLE_CLASS}>Career</h2>
         <p>Gaon Platform (2020 - 2023.6)</p>
         <p>Innogen (2017 - 2018)</p>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,7 +15,7 @@ export default function Hero() {
       />
       <h2 className="text-3xl font-bold mt-2">{"Hi, I'm Kyubin"}</h2>
       <h3 className="text-xl font-semibold">Full-stack Engineer</h3>
-      <p>꿈을 코딩하는 사람</p>
+      <p>A person coding dreams</p>
       <Link href={"/contact"}>
         <button className="bg-yellow-500 font-bold rounded-xl py-1 px-4 mt-2">
           Contact Me
